4e8b9f38c637dfbf6375cf1ea13fbdeb69248194
Erivan Barros <ErivanB53@gmail.com>
2025-11-27 18:07:06 -0300

    Initial commit, dependencias MAVEn adicionadas

---
+++ src/main/java/br/edu/ifpb/API/Cache/de/Produtos/ApiCacheDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.API.Cache.de.Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCacheDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCacheDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/API/Cache/de/Produtos/ApiCacheDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.API.Cache.de.Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCacheDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
